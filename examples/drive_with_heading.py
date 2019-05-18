7894b1038f4163aaa2d0b3eb7d14e491a5b11c29
enot <a.a.lemets@gmail.com>
2019-05-19 01:51:03 +0300

    add driving example

---
+++ examples/drive_with_heading.py
@@ -0,0 +1,25 @@
+import random
+from time import sleep
+
+from pysphero.core import Sphero
+from pysphero.driving import Direction
+
+
+def main():
+    mac_address = 'aa:bb:cc:dd:ee:ff'
+    sphero = Sphero(mac_address=mac_address)
+    sphero.power.wake()
+
+    for _ in range(5):
+        sleep(2)
+        speed = random.randint(50, 100)
+        heading = random.randint(0, 360)
+        print(f"Send drive with speed {speed} and heading {heading}")
+
+        sphero.driving.drive_with_heading(speed, heading, Direction.forward)
+
+    sphero.power.enter_soft_sleep()
+
+
+if __name__ == '__main__':
+    main()
