7a67a53d41238591d41820faf2c56c6a1a20d89d
enot <a.a.lemets@gmail.com>
2019-05-20 01:05:51 +0300

    some matrix commands

--- pysphero/user_io.py
+++ pysphero/user_io.py
@@ -13,6 +13,14 @@ class Color(NamedTuple):
         return [self.red & 0xff, self.green & 0xff, self.blue & 0xff]
 
 
+class Pixel(NamedTuple):
+    x: int = 0
+    y: int = 0
+
+    def to_list(self):
+        return [self.x & 0x07, self.y & 0x07]
+
+
 class Led(Enum):
     front_red = 0x01
     front_green = 0x02
@@ -21,6 +29,13 @@ class Led(Enum):
     back_green = 0x10
     back_blue = 0x20
     all = front_red | front_green | front_blue | back_red | back_green | back_blue
+
+
+class FrameRotation(Enum):
+    normal = 0x00
+    degrees_90 = 0x01
+    degrees_180 = 0x02
+    degrees_270 = 0x03
 
 
 class UserIOCommand(Enum):
@@ -54,7 +69,12 @@ class UserIOCommand(Enum):
     set_all_leds_32_bit_mask = 0x1a
     set_all_leds_64_bit_mask = 0x1b
     set_all_leds_8_bit_mask = 0x1c
+    set_led_matrix_pixel = 0x2d
     set_led_matrix_one_color = 0x2f
+    set_led_matrix_frame_rotation = 0x3a
+    set_led_matrix_text_scrolling = 0x3b
+    set_led_matrix_text_scrolling_notify = 0x3c
+    set_led_matrix_single_character = 0x42
 
 
 class UserIO(DeviceApiABC):
@@ -79,5 +99,82 @@ class UserIO(DeviceApiABC):
         self.request(
             command_id=UserIOCommand.set_all_leds_8_bit_mask,
             data=[leds, *front_color.to_list(), *back_color.to_list()],
+            target_id=0x11,
+        )
+
+    def set_led_matrix_one_color(self, color: Color = Color()):
+        """
+        Set matrix one color
+
+        :param Color color:
+        :return:
+        """
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_one_color,
+            data=color.to_list(),
+            target_id=0x12,
+        )
+
+    def set_led_matrix_pixel(self, pixel: Pixel, color: Color = Color()):
+        """
+        Put into pixel the color
+
+        :param Pixel pixel:
+        :param Color color:
+        :return:
+        """
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_pixel,
+            data=[*pixel.to_list(), *color.to_list()],
+            target_id=0x12,
+        )
+
+    def set_led_matrix_single_character(self, symbol: str, color: Color):
+        """
+        Print symbol on matrix
+
+        :param String symbol:
+        :param Color color:
+        :return:
+        """
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_single_character,
+            data=[*color.to_list(), ord(symbol)],
+            target_id=0x12,
+        )
+
+    def set_led_matrix_text_scrolling(self, string: str, color: Color, speed: int = 0x10, repeat: bool = True):
+        """
+        Print text on matrix
+
+        :param str string: max length 6 symbols
+        :param Color color:
+        :param int speed: max value is 0x1e (30)
+        :param bool repeat:
+        :return:
+        """
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_text_scrolling,
+            data=[
+                *color.to_list(),
+                speed % 0x1e,
+                int(repeat),
+                *[ord(c) for c in string[:7]],
+                0x00,  # end line
+            ],
+            target_id=0x12,
+        )
+
+    def set_led_matrix_text_scrolling_notify(self):
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_text_scrolling_notify,
+            data=[0x00],  # todo: unknown
+            target_id=0x11,
+        )
+
+    def set_led_matrix_frame_rotation(self, rotation: FrameRotation = FrameRotation.normal):
+        self.request(
+            command_id=UserIOCommand.set_led_matrix_frame_rotation,
+            data=[rotation.value],
             target_id=0x11,
         )
